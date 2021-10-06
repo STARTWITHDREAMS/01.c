d19f991e7fad7de09b1fc66cbfbc8d0451a8557f
STARTWITHDREAMS <1512468783@qq.com>
2021-10-06 09:56:22 +0800

    02.c

---
+++ 02.c
@@ -0,0 +1,19 @@
+//#include<stdio.h>
+int main()
+{ int a,b,c,exchange;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a>b){
+	exchange=a;a=b;b=exchange;
+	if(a>c)
+	{exchange=a;a=c;c=exchange;}
+	 if(b>c)
+	{exchange=b;b=c;c=exchange;}
+	
+	}
+	else if(a>c){
+		exchange=a;a=c;c=exchange; exchange=b;b=c;c=exchange;}
+	  else if(b>c){
+		 exchange=b;b=c;c=exchange;}
+	printf("输出三个数：%d %d %d\n",a,b,c);
+return 0;
+}
