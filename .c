4234446bf21086c31a6c35381a7c0a90c4b3182b
STARTWITHDREAMS <1512468783@qq.com>
2021-10-18 23:40:02 +0800

    04.c

---
+++ .c
@@ -0,0 +1,17 @@
+﻿1>------ 已启动生成: 项目: test.c, 配置: Debug Win32 ------
+1>生成启动时间为 2021/10/18 22:34:48。
+1>InitializeBuildStatus:
+1>  正在创建“Debug\test.c.unsuccessfulbuild”，因为已指定“AlwaysCreate”。
+1>ClCompile:
+1>  test.c
+1>Link:
+1>  LINK : 没有找到 C:\Users\Administrator\Desktop\test.c\Debug\test.c.exe 或上一个增量链接没有生成它；正在执行完全链接
+1>  test.c.vcxproj -> C:\Users\Administrator\Desktop\test.c\Debug\test.c.exe
+1>FinalizeBuildStatus:
+1>  正在删除文件“Debug\test.c.unsuccessfulbuild”。
+1>  正在对“Debug\test.c.lastbuildstate”执行 Touch 任务。
+1>
+1>生成成功。
+1>
+1>已用时间 00:00:04.02
+========== 生成: 成功 1 个，失败 0 个，最新 0 个，跳过 0 个 ==========
