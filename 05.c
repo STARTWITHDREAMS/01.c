229fe68d3cf25eaf1699f81063c34d93a189a66d
STARTWITHDREAMS <1512468783@qq.com>
2021-10-22 22:43:21 +0800

    05.c

---
+++ 05.c
@@ -0,0 +1,19 @@
+﻿1>------ 已启动生成: 项目: test.c, 配置: Debug Win32 ------
+1>生成启动时间为 2021/10/22 22:38:36。
+1>InitializeBuildStatus:
+1>  正在创建“Debug\test.c.unsuccessfulbuild”，因为已指定“AlwaysCreate”。
+1>ClCompile:
+1>  .c
+1>c:\users\administrator\desktop\test.c\test.c\.c(6): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>          c:\program files (x86)\microsoft visual studio 10.0\vc\include\stdio.h(304) : 参见“scanf”的声明
+1>Link:
+1>  LINK : 没有找到 C:\Users\Administrator\Desktop\test.c\Debug\test.c.exe 或上一个增量链接没有生成它；正在执行完全链接
+1>  test.c.vcxproj -> C:\Users\Administrator\Desktop\test.c\Debug\test.c.exe
+1>FinalizeBuildStatus:
+1>  正在删除文件“Debug\test.c.unsuccessfulbuild”。
+1>  正在对“Debug\test.c.lastbuildstate”执行 Touch 任务。
+1>
+1>生成成功。
+1>
+1>已用时间 00:00:03.94
+========== 生成: 成功 1 个，失败 0 个，最新 0 个，跳过 0 个 ==========
