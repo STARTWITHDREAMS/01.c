3cd688dd84c7b7a88adb55e94cf7a561c35d97cb
eternity <1512468793@qq.com>
2021-12-05 23:04:01 +0800

    16.c

---
+++ 16.c/16.c/main.c
@@ -0,0 +1,17 @@
+//洛谷p1044
+#include<stdio.h>
+int Fun(int n)
+{
+    int sum = 0;
+    if(n == 0) return 1;
+    for(int i = 0; i <= n - 1; i++)
+        sum += Fun(i) * Fun(n-1-i);
+    return sum;
+}
+int main()
+{
+    int n = 0;
+    scanf("%d",&n);
+    printf("%d\n",Fun(n));
+    return 0;
+}
